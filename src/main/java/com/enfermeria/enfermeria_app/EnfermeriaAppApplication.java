2b538233f8b62a1f08b955c0f7019b7d49d2d5ad
agarcia-2022075 <agarcia-2022075@kinal.edu.gt>
2026-02-15 22:30:56 -0600

    Setup: Esqueleto Spring Boot inicial (Sin features)

---
+++ src/main/java/com/enfermeria/enfermeria_app/EnfermeriaAppApplication.java
@@ -0,0 +1,13 @@
+package com.enfermeria.enfermeria_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnfermeriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnfermeriaAppApplication.class, args);
+	}
+
+}
