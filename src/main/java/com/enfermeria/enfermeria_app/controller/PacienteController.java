ee5fe3cc9dc05120944064d49addc55464a051e6
hgarcia-2023541 <hgarcia-2023541@kinal.edu.gt>
2026-02-16 23:11:13 -0600

    feat(controlador): se agrega PacienteController con endpoints GET y POST

--- src/main/java/com/enfermeria/enfermeria_app/controller/PacienteController.java
+++ src/main/java/com/enfermeria/enfermeria_app/controller/PacienteController.java
@@ -1,4 +1,28 @@
 package com.enfermeria.enfermeria_app.controller;
 
+import com.enfermeria.enfermeria_app.entity.Paciente;
+import com.enfermeria.enfermeria_app.repository.PacienteRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
+
+    private final PacienteRepository repo;
+
+    public PacienteController(PacienteRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public List<Paciente> listar() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public Paciente guardar(@RequestBody Paciente p) {
+        return repo.save(p);
+    }
 }
